2330de461bd6616787d959408efbe9b50cda8df0
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-04 23:54:18 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,6 +6,13 @@ BASE_HOA = {
 
 
 def add_character(show, name)
+  BASE_HOA = {
+  :chipmunks => ["Alvin", "Simon", "Theodore"],
+  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+  :jetsons => ["George", "Jane", "Judy", "Elroy"]
+}
+  
+  
   BASE_HOA[:chipmunks] << "Dave"
   BASE_HOA
   
